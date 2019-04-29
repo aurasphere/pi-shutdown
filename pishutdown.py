0b65dc5f076ea6d7578b8f31435493fd92140c27
Donato Rimenti <donatohan.rimenti@gmail.com>
2019-04-29 14:32:54 +0000

    Added button type configuration (normally-closed/open), added service configuration and install script, removed busy wait loop

--- pishutdown.py
+++ pishutdown.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
+
+# MIT License
+#
+# Copyright (c) 2019 Donato Rimenti
+# Copyright (c) 2019 George Ilyes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # shutdown/reboot(/power on) Raspberry Pi with pushbutton
 
 import RPi.GPIO as GPIO
 from subprocess import call
 from datetime import datetime
 import time
+from signal import pause
 
 # pushbutton connected to this GPIO pin, using pin 5 also has the benefit of
 # waking / powering up Raspberry Pi when button is pressed
@@ -16,6 +41,9 @@ shutdownMinSeconds = 3
 # button debounce time in seconds
 debounceSeconds = 0.01
 
+# whether the button is normally open or closed
+normallyOpen = False
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -25,7 +53,13 @@ buttonPressedTime = None
 def buttonStateChanged(pin):
     global buttonPressedTime
 
-    if not (GPIO.input(pin)):
+    incomingSignal = GPIO.input(pin)
+
+    # inverts the signal if the button is normally closed
+    if not (normallyOpen):
+        incomingSignal = not incomingSignal
+
+    if not (incomingSignal):
         # button is down
         if buttonPressedTime is None:
             buttonPressedTime = datetime.now()
@@ -45,6 +79,5 @@ def buttonStateChanged(pin):
 # subscribe to button presses
 GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
 
-while True:
-    # sleep to reduce unnecessary CPU usage
-    time.sleep(5)
+# wait forever
+pause()
